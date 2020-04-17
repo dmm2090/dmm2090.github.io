82667b717f31a6eec9ef544e28beb72a9f7f0889
dmm2090MM <dommorse@outlook.com>
2020-04-17 12:19:59 -0400

    .

--- js/myScripts.js
+++ js/myScripts.js
@@ -8,22 +8,23 @@ function navFunction() {
   }
 }
 
+
 //When the user scolls the page, execute stickyFunction
-window.onscroll = function() {stickyFunction()};
+//window.onscroll = function() {stickyFunction()};
 
 //Get the topNav
-var topNav = document.getElementById("topNav");
+//var topNav = document.getElementById("topNav");
 
 //Get the offset position of topNav
-var sticky = topNav.offsetTop;
+//var sticky = topNav.offsetTop;
 
 //Add the sticky class to the topNav when you reach its scroll position.
 //Remove "sticky" when you leave the scroll position
-function stickyFunction() {
-  if (window.pageYOffset >= sticky) {
-    topNav.classList.add("sticky")
-  }
-  else {
-    topNav.classList.remove("sticky");
-  }
-}
+//function stickyFunction() {
+  //if (window.pageYOffset >= sticky) {
+    //topNav.classList.add("sticky")
+  //}
+  //else {
+    //topNav.classList.remove("sticky");
+  //}
+//}
