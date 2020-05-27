943f102fc87543e6987821142cd312c7c527013d
dmm2090 <dmm2090@email.vccs.edu>
2020-05-26 20:27:51 -0400

    .

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,30 +1,9 @@
 function navFunction() {
   var x = document.getElementById("mainTopNav");
-  if (x.className === "topNav") {
+  if (x.className === "nav") {
     x.className += " responsive";
   }
   else {
-    x.className = "topNav";
+    x.className = "nav";
   }
 }
-
-
-//When the user scolls the page, execute stickyFunction
-//window.onscroll = function() {stickyFunction()};
-
-//Get the topNav
-//var topNav = document.getElementById("topNav");
-
-//Get the offset position of topNav
-//var sticky = topNav.offsetTop;
-
-//Add the sticky class to the topNav when you reach its scroll position.
-//Remove "sticky" when you leave the scroll position
-//function stickyFunction() {
-  //if (window.pageYOffset >= sticky) {
-    //topNav.classList.add("sticky")
-  //}
-  //else {
-    //topNav.classList.remove("sticky");
-  //}
-//}
